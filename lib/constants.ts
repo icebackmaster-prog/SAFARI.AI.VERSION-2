66f1cde3aa15cc0c7e9714f0b8cf102e8750177e
iceback master tech <icebackmaster@gmail.com>
2026-07-15 22:04:32 +0200

    Add constants for app information and support

---
+++ lib/constants.ts
@@ -0,0 +1,11 @@
+export const APP_NAME = "SAFARI AI V2";
+
+export const COMPANY = "SAFARI TECHNOLOGY";
+
+export const OWNER = "ICEBACK MASTER TECH";
+
+export const SUPPORT_EMAIL =
+  "safaritechcompany@gmail.com";
+
+export const OWNER_PHONE =
+  "+263788377887";
