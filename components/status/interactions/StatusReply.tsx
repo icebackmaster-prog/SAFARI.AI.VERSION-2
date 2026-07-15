b404e879732d1f5a68e1e9eadedf0729d4e63a86
iceback master tech <icebackmaster@gmail.com>
2026-07-15 21:25:03 +0200

    Create StatusReply.tsx

---
+++ components/status/interactions/StatusReply.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function StatusReply() {
+  return (
+    <div className="status-reply">
+      <input
+        type="text"
+        placeholder="Reply to status..."
+      />
+
+      <button>Send</button>
+    </div>
+  );
+}
