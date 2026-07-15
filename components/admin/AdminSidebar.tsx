7f235783873afdb8d9f45566213ffdc49eae7f0d
iceback master tech <icebackmaster@gmail.com>
2026-07-15 20:16:38 +0200

    Create AdminSidebar.tsx

---
+++ components/admin/AdminSidebar.tsx
@@ -0,0 +1,33 @@
+export default function AdminSidebar() {
+
+return(
+
+<aside className="admin-sidebar">
+
+<ul>
+
+<li>Dashboard</li>
+
+<li>Users</li>
+
+<li>Status</li>
+
+<li>Premium</li>
+
+<li>Notifications</li>
+
+<li>Reports</li>
+
+<li>Settings</li>
+
+<li>Storage</li>
+
+<li>Logs</li>
+
+</ul>
+
+</aside>
+
+);
+
+}
