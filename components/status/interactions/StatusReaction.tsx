c1d84e1c00aef6e53f24b2049c6abd8fb6ab83a4
iceback master tech <icebackmaster@gmail.com>
2026-07-15 21:23:59 +0200

    Create StatusReaction.tsx

---
+++ components/status/interactions/StatusReaction.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function StatusReaction() {
+  return (
+    <div className="status-reaction">
+      <button>❤️ Like</button>
+      <button>🔥 Fire</button>
+      <button>👏 Clap</button>
+      <button>😂 Laugh</button>
+    </div>
+  );
+}
