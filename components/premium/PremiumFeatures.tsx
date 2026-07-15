f5c8ae67861b74123e498dcbeee448b46ab6ad9e
iceback master tech <icebackmaster@gmail.com>
2026-07-15 21:53:43 +0200

    Add PremiumFeatures component with feature list

---
+++ components/premium/PremiumFeatures.tsx
@@ -0,0 +1,23 @@
+export default function PremiumFeatures() {
+  return (
+    <div className="premium-features">
+
+      <h2>Premium Features</h2>
+
+      <ul>
+
+        <li>Unlimited AI Chat</li>
+
+        <li>Unlimited Image Upload</li>
+
+        <li>Unlimited Video Upload</li>
+
+        <li>Priority Responses</li>
+
+        <li>Early Access Features</li>
+
+      </ul>
+
+    </div>
+  );
+}
