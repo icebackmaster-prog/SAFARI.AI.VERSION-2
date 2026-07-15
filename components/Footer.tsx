29d07d3dea6cfc455a1c4c71d87687ab4ea4b428
iceback master tech <icebackmaster@gmail.com>
2026-07-15 15:51:20 +0200

    Add Footer component with creator information

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>Created by ICEBACK MASTER TECH</p>
+
+      <p>
+        Powered by ICEBACK MASTER TECH &
+        SAFARI TECHNOLOGY
+      </p>
+    </footer>
+  );
+}
