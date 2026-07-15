da20d2e3d0c2a3419fa235f155122e28bed2a6a6
iceback master tech <icebackmaster@gmail.com>
2026-07-15 21:18:41 +0200

    Create OwnerPage component with various subcomponents

---
+++ app/owner/page.tsx
@@ -0,0 +1,24 @@
+import OwnerPanel from "@/components/owner/OwnerPanel";
+import OwnerStats from "@/components/owner/OwnerStats";
+import StatusUploader from "@/components/owner/StatusUploader";
+import RecentActivities from "@/components/owner/RecentActivities";
+
+export default function OwnerPage(){
+
+return(
+
+<main>
+
+<OwnerPanel/>
+
+<OwnerStats/>
+
+<StatusUploader/>
+
+<RecentActivities/>
+
+</main>
+
+);
+
+}
