629f42fe42950ce6a68536a9ad0f31f0af30a1eb
iceback master tech <icebackmaster@gmail.com>
2026-07-15 20:57:36 +0200

    Add FeedbackPage component with feedback form

---
+++ app/feedback/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+export default function FeedbackPage() {
+
+return(
+
+<main>
+
+<h1>💬 Feedback</h1>
+
+<textarea
+placeholder="Send feedback..."
+></textarea>
+
+<button>
+
+Send Feedback
+
+</button>
+
+</main>
+
+);
+
+}
