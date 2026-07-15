a990d92adf9ac14f82e35a3b08b807e4b9fa17a1
iceback master tech <icebackmaster@gmail.com>
2026-07-15 15:46:42 +0200

    Create page.tsx

---
+++ app/login/page.tsx
@@ -0,0 +1,16 @@
+export default function LoginPage() {
+  return (
+    <main>
+      <h1>Login</h1>
+
+      <input placeholder="Email" />
+
+      <input
+        type="password"
+        placeholder="Password"
+      />
+
+      <button>Login</button>
+    </main>
+  );
+}
