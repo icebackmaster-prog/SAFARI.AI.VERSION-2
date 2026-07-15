8fa00823607d31f815c757af1f52c16dfdf64c7b
iceback master tech <icebackmaster@gmail.com>
2026-07-15 20:46:41 +0200

    Create policies.sql

---
+++ database/policies.sql
@@ -0,0 +1,6 @@
+alter table profiles enable row level security;
+alter table status enable row level security;
+alter table conversations enable row level security;
+alter table messages enable row level security;
+alter table notifications enable row level security;
+alter table feedback enable row level security;
