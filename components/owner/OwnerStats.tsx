2ecfcb9de9eb90a09fbda6fa400ea3f33f6b877e
iceback master tech <icebackmaster@gmail.com>
2026-07-15 21:16:55 +0200

    Create OwnerStats component for user statistics
    Initial implementation of OwnerStats component displaying user statistics.

---
+++ components/owner/OwnerStats.tsx
@@ -0,0 +1,35 @@
+export default function OwnerStats(){
+
+return(
+
+<div className="owner-stats">
+
+<div>
+
+<h3>Total Users</h3>
+
+<p>0</p>
+
+</div>
+
+<div>
+
+<h3>Status Uploaded</h3>
+
+<p>0</p>
+
+</div>
+
+<div>
+
+<h3>Premium Users</h3>
+
+<p>0</p>
+
+</div>
+
+</div>
+
+);
+
+}
