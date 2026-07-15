4335e1be9518df530b7e026dde39580c75d922e3
iceback master tech <icebackmaster@gmail.com>
2026-07-15 15:47:36 +0200

    Create page.tsx

---
+++ app/signup/page.tsx
@@ -0,0 +1,16 @@
+export default function SignupPage() {
+  return (
+    <main>
+      <h1>Create Account</h1>
+
+      <input placeholder="Email" />
+
+      <input
+        type="password"
+        placeholder="Password"
+      />
+
+      <button>Create Account</button>
+    </main>
+  );
+}
