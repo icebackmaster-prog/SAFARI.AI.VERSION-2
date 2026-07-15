8b4c7b116dccd92bfb44fff01e93423cb63c0732
iceback master tech <icebackmaster@gmail.com>
2026-07-15 15:51:58 +0200

    Create Sidebar.tsx

---
+++ components/Sidebar.tsx
@@ -0,0 +1,14 @@
+export default function Sidebar() {
+  return (
+    <aside>
+      <ul>
+        <li>Home</li>
+        <li>Chat</li>
+        <li>Dashboard</li>
+        <li>Status</li>
+        <li>Premium</li>
+        <li>Settings</li>
+      </ul>
+    </aside>
+  );
+}
