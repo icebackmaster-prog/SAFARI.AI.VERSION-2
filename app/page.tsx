474b62ef34cb283405c55c114fa1c0bfbc5a783b
iceback master tech <icebackmaster@gmail.com>
2026-07-15 15:22:27 +0200

    Add Home component for SAFARI AI V2

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>🦁 SAFARI AI V2</h1>
+
+      <p>Created by ICEBACK MASTER TECH</p>
+
+      <p>Powered by ICEBACK MASTER TECH & SAFARI TECHNOLOGY</p>
+
+      <p>Welcome to SAFARI AI V2.</p>
+    </main>
+  );
+}
